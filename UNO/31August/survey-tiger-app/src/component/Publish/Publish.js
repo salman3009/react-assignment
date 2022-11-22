562071707b678d6ab56bcb50f641413ce255d1fa
salman3009 <salman444g@gmail.com>
2022-11-22 20:26:51 +0530

    code

--- UNO/31August/survey-tiger-app/src/component/Publish/Publish.js
+++ UNO/31August/survey-tiger-app/src/component/Publish/Publish.js
@@ -1,9 +1,28 @@
 import './Publish.css';
 
-const Publish = ({questionCreation})=>{
+const Publish = ({ questionCreation }) => {
     console.log(questionCreation);
     return (<div>
-        Hello publish
+        <div className='container'>
+            {questionCreation.map((object, index) => {
+                return (<div className="row" key={index}>
+                    <div className="col-3">
+
+                    </div>
+                    <div className="col-6">
+                        <h1>{object.question}</h1>
+                        {object.options.map((input) => {
+                            return ( <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" value="option1" />
+                            <label className="form-check-label" for="inlineRadio1">{input}</label>
+                        </div>)   
+                        })}
+                    </div>
+                    <div className="col-3">
+                    </div>
+                </div>)
+            })}
+        </div>
     </div>)
 }
 
