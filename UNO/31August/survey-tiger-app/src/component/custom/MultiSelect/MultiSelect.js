b5e8ca930c24fa1a47caa4b6833fa26b0ab41eea
salman3009 <salman444g@gmail.com>
2022-11-23 19:52:13 +0530

    code

--- UNO/31August/survey-tiger-app/src/component/custom/MultiSelect/MultiSelect.js
+++ UNO/31August/survey-tiger-app/src/component/custom/MultiSelect/MultiSelect.js
@@ -5,15 +5,26 @@ const MultiSelect=()=>{
 
     const [getList,setList] = useState({
         question:'',
-        options:[{id:0,value:''}]
+        options:[{id:1,value:''},{id:2,value:''}]
     });
 
     useEffect(()=>{
        console.log(getList);
-    },[getList])
+    },[getList]);
 
     const onChangeHandler=(event)=>{
         setList({...getList,[event.target.name]:event.target.value})
+    }
+
+    const onAdditionHandler=()=>{
+        if(getList.options.length===4){
+            return;
+        }
+        let count = getList.options.length;
+        let object ={id:count+1,value:''};
+        let list = [...getList.options];
+        list = [...list,object];
+        setList({...getList,options:list});
     }
 
     return (<div className="container">
@@ -28,17 +39,19 @@ const MultiSelect=()=>{
                     <input type="text" class="form-control"  name="question" onChange={onChangeHandler} />
                 </div>
             </div>
-            <div class="col-auto">
+            {getList.options.map((obj,index)=>{
+                return(<div key={index} class="col-auto">
                 <div class="input-group mb-2">
                     <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username" />
                     <div class="input-group-prepend">
-                        <div class="input-group-text">+</div>
+                        <div class="input-group-text" onClick={onAdditionHandler}>+</div>
                     </div>
                     <div class="input-group-prepend">
                         <div class="input-group-text">-</div>
                     </div>
                 </div>
-            </div>
+            </div>)
+            })}
         </div>
         <div className="col-3"></div>
     </div>
