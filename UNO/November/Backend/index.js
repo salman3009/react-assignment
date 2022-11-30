2a5dd4c07ffb193ad4e30714a3490e4c1d5bf962
salman3009 <salman444g@gmail.com>
2022-11-30 19:27:13 +0530

    code

--- UNO/November/Backend/index.js
+++ UNO/November/Backend/index.js
@@ -3,7 +3,24 @@ const app = exp();
 
 console.log("hello world");
 
-app.listen(3020,()=>{
+app.get('',(req,res)=>{
+        res.status(200).json({
+            message:"succesfully data is sent" 
+        })
+});
+
+app.post('',(req,res)=>{
+     console.log(req.body);
+     res.status(201).json({
+        message:"added data"
+     })
+});
+
+app.get('/index',(req,res)=>{
+      res.send('<div><h1>Welcome to Nodejs</h1><h2>Full stack application</h2></div>')
+});
+
+app.listen(8080,()=>{
     console.log("server is running on port 3020")
 });
 
