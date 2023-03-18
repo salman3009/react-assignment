9c9ffa919540b44969b2363714e5dee43da65e23
salman3009 <salman444g@gmail.com>
2023-03-18 12:00:07 +0530

    code

--- August/18-03-23/basic-assignment/src/Form.js
+++ August/18-03-23/basic-assignment/src/Form.js
@@ -1,10 +1,20 @@
-
+import {useRef} from 'react';
 
 const Form=()=>{
+      
+      const fullName = useRef(null);
+
+      const date = useRef(null);
+
+       const onSubmitHandler=()=>{
+           alert("submit");
+           console.log(fullName.current.value);
+           console.log(date.current.value);
+       }
        return (<div>
-          <input type="text" name="details"/>
-          <input type="date"/>
-          <input type="submit"/>
+          <input type="text" name="details" ref={fullName}/>
+          <input type="date" ref={date}/>
+          <input onClick={onSubmitHandler} type="submit"/>
        </div>)
 }
 export default Form;
--- August/18-03-23/basic-assignment/src/List.js
+++ August/18-03-23/basic-assignment/src/List.js
@@ -3,7 +3,7 @@ const List=(props)=>{
        return (<div>
          {
               props.filterList.map((obj,index)=>{
-                    return (<div>
+                    return (<div key={index}>
                      <h1>{obj.name}</h1>
                      <h1>{obj.date.toLocaleDateString()}</h1>
                     </div>)
