f1fd2b8d71c17a4d628e56da4e9b4374163049c0
salman3009 <salman444g@gmail.com>
2023-03-18 12:24:30 +0530

    code

--- August/18-03-23/basic-assignment/src/App.js
+++ August/18-03-23/basic-assignment/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useRef } from "react";
 import Form  from "./Form";
 import List from "./List";
 
@@ -10,20 +10,35 @@ const list=[{
 
 const App=()=>{
 
-       const [getList,setList] = useState(list)
+       const [getList,setList] = useState(list);
+
+       const firstDate = useRef(null);
+       const SecondDate = useRef(null);
 
        const appendDetails=(obj)=>{
             setList([...getList,obj]);
        } 
 
+       const calculateDate=()=>{
+            let first = new Date(firstDate.current.value);
+            let second = new Date(SecondDate.current.value);
+            let result = Math.abs(second-first);
+            let finalResult = Math.ceil((result/(1000*60*60*24)));
+            console.log(finalResult);
+       }
+
        return(<div>
         <Form changeHandler={appendDetails}/>
         <List filterList={getList}/>
+        
         {2+2}
         {list.length>0?"yes":"no"}
         {list.length && (<div>
               <h1>Hello world</h1>
         </div>)}
+        First Date:<input type="date" ref={firstDate}/>
+        second Date:<input type="date" ref={SecondDate}/>
+        <button onClick={calculateDate}>Date Function</button>
        </div>)
 }
 export default App;
